fb92cb5ff9efbdc5a0da3583b9ba67f29ea00646
Marcus Hoepfner <marcus.hoepfner@sap.com>
2017-06-09 09:37:49 +0000

    [FEATURE] switch device dialog contains qr code

--- cart/webapp/controller/BaseController.js
+++ cart/webapp/controller/BaseController.js
@@ -1,84 +1,110 @@
 sap.ui.define(
 	["sap/ui/core/mvc/Controller",
-	'sap/m/MessageBox'],
-	function (Controller, MessageBox) {
-	"use strict";
+		'sap/m/MessageBox',
+		'sap/m/Dialog',
+		'sap/m/Image',
+		'sap/m/Button'
+	],
+	function(Controller, MessageBox, Dialog, Image, Button) {
+		"use strict";
 
-	return Controller.extend("sap.ui.demo.cart.controller.BaseController", {
+		return Controller.extend("sap.ui.demo.cart.controller.BaseController", {
 
-		/**
-		 * Convenience method for accessing the router.
-		 * @public
-		 * @returns {sap.ui.core.routing.Router} the router for this component
-		 */
-		getRouter: function () {
-			return sap.ui.core.UIComponent.getRouterFor(this);
-		},
+			/**
+			 * Convenience method for accessing the router.
+			 * @public
+			 * @returns {sap.ui.core.routing.Router} the router for this component
+			 */
+			getRouter: function() {
+				return sap.ui.core.UIComponent.getRouterFor(this);
+			},
 
-		/**
-		 * Convenience method for getting the view model by name.
-		 * @public
-		 * @param {string} [sName] the model name
-		 * @returns {sap.ui.model.Model} the model instance
-		 */
-		getModel: function (sName) {
-			return this.getView().getModel(sName);
-		},
+			/**
+			 * Convenience method for getting the view model by name.
+			 * @public
+			 * @param {string} [sName] the model name
+			 * @returns {sap.ui.model.Model} the model instance
+			 */
+			getModel: function(sName) {
+				return this.getView().getModel(sName);
+			},
 
-		/**
-		 * Convenience method for setting the view model.
-		 * @public
-		 * @param {sap.ui.model.Model} oModel the model instance
-		 * @param {string} sName the model name
-		 * @returns {sap.ui.mvc.View} the view instance
-		 */
-		setModel: function (oModel, sName) {
-			return this.getView().setModel(oModel, sName);
-		},
+			/**
+			 * Convenience method for setting the view model.
+			 * @public
+			 * @param {sap.ui.model.Model} oModel the model instance
+			 * @param {string} sName the model name
+			 * @returns {sap.ui.mvc.View} the view instance
+			 */
+			setModel: function(oModel, sName) {
+				return this.getView().setModel(oModel, sName);
+			},
 
-		/**
-		 * Getter for the resource bundle.
-		 * @public
-		 * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
-		 */
-		getResourceBundle: function () {
-			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
-		},
-		
-		onSwitchDeviceButtonPress: function(){
-			var oCartModel = this.getView().getModel("cartProducts");
+			/**
+			 * Getter for the resource bundle.
+			 * @public
+			 * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
+			 */
+			getResourceBundle: function() {
+				return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			},
 
-			//all relevant cart properties are set back to default. Content is deleted.
-			var cartEntries = oCartModel.getProperty("/cartEntries");
-			var aProducts = [];
-			var aQuantities = [];
+			onSwitchDeviceButtonPress: function() {
+				var oCartModel = this.getView().getModel("cartProducts");
 
-			for (var entry in cartEntries){
-				if (!cartEntries.hasOwnProperty(entry)) {
-					continue;
+				//all relevant cart properties are set back to default. Content is deleted.
+				var cartEntries = oCartModel.getProperty("/cartEntries");
+				var aProducts = [];
+				var aQuantities = [];
+
+				for (var entry in cartEntries) {
+					if (!cartEntries.hasOwnProperty(entry)) {
+						continue;
+					}
+					aProducts.push(cartEntries[entry]["ProductId"]);
+					aQuantities.push(cartEntries[entry]["Quantity"]);
 				}
-				aProducts.push(cartEntries[entry]["ProductId"]);
-				aQuantities.push(cartEntries[entry]["Quantity"]);
+
+				var currentHash = sap.ui.core.routing.HashChanger.getInstance().getHash();
+
+				// we add all products, all quanitites and the current hash to the url
+				// refer to WelcomeController._loadCardFromUrl for resolvement
+				var oParameters = {
+					"query": {
+						"products": aProducts.join(","),
+						"quantities": aQuantities.join(","),
+						"navTarget": encodeURIComponent(currentHash)
+					}
+				};
+
+				// the url
+				var sUrl = window.location.origin + window.location.pathname + "#/" + this.getRouter().getURL("home", oParameters);
+
+				var sEncodedUrl = encodeURIComponent(sUrl);
+
+				// var qrUrl
+				// we use google chart api to generate qr code: https://developers.google.com/chart/infographics/docs/qr_codes
+				var qrUrl = "https://chart.googleapis.com/chart?cht=qr&chs=400x300&chl=" + sEncodedUrl;
+
+				// opening the dialog
+				var qrDialog = new Dialog({
+					title: 'Switch Device',
+					content: new Image({
+						src: qrUrl
+					}),
+					beginButton: new Button({
+						text: 'Close',
+						press: function() {
+							qrDialog.close();
+						}.bind(this)
+					})
+				});
+
+				//to get access to the global model
+				this.getView().addDependent(qrDialog);
+
+				qrDialog.open();
 			}
-			
-			var currentHash = sap.ui.core.routing.HashChanger.getInstance().getHash();
-			
-			// we add all products, all quanitites and the current hash to the url
-			// refer to WelcomeController._loadCardFromUrl for resolvement
-			var oParameters = { 
-				"query" : {
-					"products":aProducts.join(","),
-					"quantities":aQuantities.join(","),
-					"navTarget":encodeURIComponent(currentHash)
-				}
-			};
-			
-			var sUrl = window.location.origin + 
-					window.location.pathname + 
-					"#/" + 
-					this.getRouter().getURL("home", oParameters);
-			MessageBox.show("URL: " + sUrl);
-		}
 
+		});
 	});
-});
